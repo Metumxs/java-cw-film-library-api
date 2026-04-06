25739ecfc926f2f00174eb9afa9350cccd29045e
Metumxs <fromuash@gmail.com>
2026-04-06 17:28:21 +0300

    no message

--- src/main/java/com/metumxs/filmlibraryapi/FilmLibraryApiApplication.java
+++ src/main/java/com/metumxs/filmlibraryapi/FilmLibraryApiApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FilmLibraryApiApplication {
+public class FilmLibraryApiApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(FilmLibraryApiApplication.class, args);
-	}
-
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FilmLibraryApiApplication.class, args);
+    }
 }
