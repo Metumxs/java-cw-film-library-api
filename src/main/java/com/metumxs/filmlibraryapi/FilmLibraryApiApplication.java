f40126949a86a30254a45478c13d5e6ce54e1372
Metumxs <fromuash@gmail.com>
2026-04-17 17:40:09 +0300

    feat(docker): containerize application and postgres with docker compose

--- src/main/java/com/metumxs/filmlibraryapi/FilmLibraryApiApplication.java
+++ src/main/java/com/metumxs/filmlibraryapi/FilmLibraryApiApplication.java
@@ -2,8 +2,12 @@ package com.metumxs.filmlibraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class FilmLibraryApiApplication
 {
 
--- src/main/resources/db/migration/V4__update_admin_user_role.sql
+++ src/main/resources/db/migration/V4__update_admin_user_role.sql
@@ -1,3 +1,10 @@
-UPDATE users
-SET role_id = (SELECT id FROM roles WHERE name = 'ADMIN')
-WHERE email = 'admin@example.com';
+INSERT INTO users (name, email, password_hash, role_id)
+VALUES (
+            'admin_1',
+           'admin@example.com',
+            '$2a$12$xy7YcvIM1YFAlEF6oVaWbOCrOQPDEUmjkmDvCJF0XIvfigUcxJK32',
+           (SELECT id FROM roles WHERE name = 'ADMIN')
+       )
+ON CONFLICT (email)
+    DO UPDATE SET
+    role_id = (SELECT id FROM roles WHERE name = 'ADMIN');
