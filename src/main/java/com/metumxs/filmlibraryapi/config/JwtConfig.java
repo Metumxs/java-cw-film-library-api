d9b77aa10c574f7006648c726cd48d5cd28bcd11
Metumxs <fromuash@gmail.com>
2026-04-20 13:40:46 +0300

    no message

--- src/main/java/com/metumxs/filmlibraryapi/config/JwtConfig.java
+++ src/main/java/com/metumxs/filmlibraryapi/config/JwtConfig.java
@@ -1,22 +1,17 @@
 package com.metumxs.filmlibraryapi.config;
 
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.jwk.JWKMatcher;
-import com.nimbusds.jose.jwk.JWKSelector;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
@@ -24,14 +19,21 @@ import java.util.UUID;
 @Configuration
 public class JwtConfig
 {
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
+
     @Bean
     public JWKSource<SecurityContext> jwkSource()
     {
-        RSAKey rsaKey = generateRsaKey();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
 
-        JWKSet jwkSet = new JWKSet(rsaKey);
-
-        return new ImmutableJWKSet<>(jwkSet);
+        return new ImmutableJWKSet<>(new JWKSet(rsaKey));
     }
 
     @Bean
@@ -41,43 +43,8 @@ public class JwtConfig
     }
 
     @Bean
-    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource)
+    public JwtDecoder jwtDecoder()
     {
-        try
-        {
-            RSAKey rsaKey = (RSAKey) jwkSource.get(
-                    new JWKSelector(new JWKMatcher.Builder().build()),
-                    null
-            ).getFirst();
-
-            return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey()).build();
-        }
-        catch (JOSEException exception)
-        {
-            throw new IllegalStateException("Failed to initialize JWT decoder with RSA public key", exception);
-        }
-    }
-
-    private RSAKey generateRsaKey()
-    {
-        try
-        {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-            keyPairGenerator.initialize(2048);
-
-            KeyPair keyPair = keyPairGenerator.generateKeyPair();
-
-            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-            RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-
-            return new RSAKey.Builder(publicKey)
-                    .privateKey(privateKey)
-                    .keyID(UUID.randomUUID().toString())
-                    .build();
-        }
-        catch (NoSuchAlgorithmException exception)
-        {
-            throw new IllegalStateException("Failed to generate RSA key", exception);
-        }
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
     }
 }
